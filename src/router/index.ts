abcef04fdd2a56a0709774973833ec662db41a70
PC <tobias.irigoyen@gmail.com>
2026-08-03 17:23:44 -0300

    (setup): router first approach with all empty views. Change Vue.js version for a previous one

---
+++ src/router/index.ts
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'splash',
+      component: () => import('@/views/Splash.vue'),
+    },
+    {
+      path: '/onboarding-1',
+      name: 'onboarding-1',
+      component: () => import('@/views/OnBoardingOne.vue'),
+    },
+    {
+      path: '/onboarding-2',
+      name: 'onboarding-2',
+      component: () => import('@/views/OnBoardingTwo.vue'),
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: () => import('@/views/Favorites.vue'),
+    },
+    {w
+      path: '/regions',
+      name: 'regions',
+      component: () => import('@/views/Regions.vue'),
+    },
+    {
+      path: '/pokemons',
+      name: 'pokemons',
+      component: () => import('@/views/Pokemons.vue'),
+    },
+    {
+      path: '/pokemon/:id',
+      name: 'pokemon-detail',
+      component: () => import('@/views/PokemonDetail.vue'),
+      props: true,
+    },
+  ],
+})
+
+export default router
