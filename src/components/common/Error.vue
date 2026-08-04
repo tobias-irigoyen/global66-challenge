d3864f4d127d45598a6c8256b1a22171ba614f11
PC <tobias.irigoyen@gmail.com>
2026-08-04 10:57:30 -0300

    (error): add error component

---
+++ src/components/common/Error.vue
@@ -0,0 +1,52 @@
+<template>
+  <article class="flex flex-col justify-end items-center">
+
+  <div class="mb-8 h-[215px] w-[270px]">
+    <div
+      v-if="!loadedImage"
+      class="w-full h-full animate-pulse bg-gray-300 rounded-xl"
+    />
+    
+    <img 
+      :src="image"
+      :alt="title"
+      @load="loadedImage = true"
+      class="mx-auto mb-8"
+      :class="['w-full h-full object-contain transition-opacity duration-300', loadedImage ? 'opacity-100' : 'opacity-0'
+      ]"
+    />
+  </div>
+
+    <h1 class="text-[1.625rem] font-medium text-center mb-4">
+      {{ title }}
+    </h1>
+
+    <p class="text-center text-sm mb-8 text-[var(--secondary-text)] md:max-w-[80%]">
+      {{ description }}
+    </p>
+
+    <button
+      class="font-semibold bg-[var(--primary-btn)] hover:bg-[var(--checked-btn)] cursor-pointer w-full text-white py-4 rounded-full max-w-[620px] transition-colors"
+      @click="emit('action')"
+    >
+      {{ buttonText }}
+    </button>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const loadedImage = ref(false)
+
+defineProps<{
+  image: string
+  title: string
+  description: string
+  buttonText: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'action'): void
+}>()
+</script>
--- src/views/Pokemons.vue
+++ src/views/Pokemons.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <h1>Pokemons</h1>
-  </div>
+  <section class="h-[calc(100vh-225px)] flex items-end justify-center md:items-center">
+<Error 
+      image="/src/assets/commons/magikarp.svg"
+      title="Algo salió mal..."
+      description="No pudimos cargar la información en este momento. Verifica tu conexión o intenta nuevamente más tarde."
+      buttonText="Reintentar"
+      @action="() => {}"
+    />
+</section>
 </template>
 
 <script setup lang="ts">
-
+import Error from '../components/common/Error.vue'
 </script>
