f65df14e9e6207fe5b913bb68cfd97097ca0f40a
PC <tobias.irigoyen@gmail.com>
2026-08-06 13:30:55 -0300

    (pokemon-detail): create icon button component, apply share top nav button, create data copy function

---
+++ src/components/common/IconButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="p-2 cursor-pointer" @click="emit('click')">
+    <component :is="icon" />
+    <span class="sr-only">{{ hiddenLabel }}</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { Component } from "vue";
+
+defineProps<{
+  icon: Component;
+  hiddenLabel: string;
+}>();
+
+const emit = defineEmits<{
+  click: [];
+}>();
+</script>
--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,5 +1,4 @@
 <template>
-  
   <Loading v-if="pokemonStore.loading" />
 
   <Feedback
@@ -13,58 +12,74 @@
   />
 
   <section
-  v-else-if="pokemonStore.pokemon"
-  class="flex justify-center px-0 pb-6 !mt-[-60px] !pt-[330px] relative md:items-center md:min-h-screen md:!pt-0"
->
-<nav class="h-[60px] w-full fixed top-0 left-0 right-0 z-50" :class="theme.badge">
-  <RouterLink to="/pokemons">
-      <ChevronLeft class="fixed top-4 left-4 z-50" />
-      <span class="sr-only">Volver</span>
-    </RouterLink>
-  <button class="fixed top-4 right-4 z-50 cursor-pointer">
-    <FavButton :pokemon="pokemonStore.pokemon" />
-  </button>
-</nav>
+    v-else-if="pokemonStore.pokemon"
+    class="flex justify-center px-0 pb-6 !mt-[-60px] !pt-[330px] relative md:items-center md:min-h-screen md:!pt-0"
+  >
+    <nav
+      class="h-[60px] w-full fixed top-0 left-0 right-0 z-50"
+      :class="theme.badge"
+    >
+      <ul class="flex justify-between items-center px-4 m-0 h-full">
+        <li>
+          <RouterLink to="/pokemons">
+            <ChevronLeft />
+            <span class="sr-only">Volver</span>
+          </RouterLink>
+        </li>
+        <li>
+          <ul class="flex justify-end items-center gap-4">
+            <li><FavButton :pokemon="pokemonStore.pokemon" /></li>
+            <li>
+              <IconButton
+                :icon="ShareIcon"
+                :hidden-label="'Compartir'"
+                @click="copyPokemonInfo()"
+              />
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
     <article class="max-w-md w-full">
       <div
-  class="flex justify-center rounded-bl-full rounded-br-full p-4 mb-6 absolute top-0 left-1/2 -translate-x-1/2 w-[100vw] z-[1] md:hidden"
-  :class="theme.badge"
->
-  <div class="relative z-10 w-[220px] h-[220px] mt-[60px]">
-    <div
-      v-if="!loadedPokemonImage"
-      class="absolute inset-0 rounded-full bg-gray-300 animate-pulse"
-    />
+        class="flex justify-center rounded-bl-full rounded-br-full p-4 mb-6 absolute top-0 left-1/2 -translate-x-1/2 w-[100vw] z-[1] md:hidden"
+        :class="theme.badge"
+      >
+        <div class="relative z-10 w-[220px] h-[220px] mt-[60px]">
+          <div
+            v-if="!loadedPokemonImage"
+            class="absolute inset-0 rounded-full bg-gray-300 animate-pulse"
+          />
 
-    <img
-      :src="pokemonStore.pokemon.image"
-      :alt="pokemonStore.pokemon.name"
-      class="relative z-10 w-full h-full object-contain transition-opacity duration-200 mt-[50px]"
-      :class="loadedPokemonImage ? 'opacity-100' : 'opacity-0'"
-      @load="loadedPokemonImage = true"
-    />
-  </div>
-</div>
+          <img
+            :src="pokemonStore.pokemon.image"
+            :alt="pokemonStore.pokemon.name"
+            class="relative z-10 w-full h-full object-contain transition-opacity duration-200 mt-[50px]"
+            :class="loadedPokemonImage ? 'opacity-100' : 'opacity-0'"
+            @load="loadedPokemonImage = true"
+          />
+        </div>
+      </div>
 
       <div
-  class="hidden md:flex justify-center items-center p-2 w-[180px] h-[180px] rounded-full mb-4"
-  :class="theme.badge"
->
-  <div class="relative z-10 w-[145px] h-[145px]">
-    <div
-      v-if="!loadedPokemonImage"
-      class="absolute inset-0 rounded-full bg-gray-300 animate-pulse"
-    />
+        class="hidden md:flex justify-center items-center p-2 w-[180px] h-[180px] rounded-full mb-4"
+        :class="theme.badge"
+      >
+        <div class="relative z-10 w-[145px] h-[145px]">
+          <div
+            v-if="!loadedPokemonImage"
+            class="absolute inset-0 rounded-full bg-gray-300 animate-pulse"
+          />
 
-    <img
-      :src="pokemonStore.pokemon.image"
-      :alt="pokemonStore.pokemon.name"
-      class="relative z-10 w-full h-full object-contain transition-opacity duration-200"
-      :class="loadedPokemonImage ? 'opacity-100' : 'opacity-0'"
-      @load="loadedPokemonImage = true"
-    />
-  </div>
-</div>
+          <img
+            :src="pokemonStore.pokemon.image"
+            :alt="pokemonStore.pokemon.name"
+            class="relative z-10 w-full h-full object-contain transition-opacity duration-200"
+            :class="loadedPokemonImage ? 'opacity-100' : 'opacity-0'"
+            @load="loadedPokemonImage = true"
+          />
+        </div>
+      </div>
 
       <div>
         <h1 class="text-3xl font-bold text-[var(--primary-text)] capitalize">
@@ -101,10 +116,30 @@
         </div>
 
         <div class="mt-8 space-y-4 grid grid-cols-2 gap-4">
-          <PokemonData :icon="WeightIcon" :label="'Peso'" :value="pokemonStore.pokemon.weight" :unit="'kg'" />
-          <PokemonData :icon="HeightIcon" :label="'Altura'" :value="pokemonStore.pokemon.height" :unit="'m'" />
-          <PokemonData :icon="CategoryIcon" :label="'Categoría'" :value="pokemonStore.pokemon.category.replace('Pokémon', '')" :unit="''" />
-          <PokemonData :icon="HabilityIcon" :label="'Habilidad'" :value="pokemonStore.pokemon.abilities[0]" :unit="''" />
+          <PokemonData
+            :icon="WeightIcon"
+            :label="'Peso'"
+            :value="pokemonStore.pokemon.weight"
+            :unit="'kg'"
+          />
+          <PokemonData
+            :icon="HeightIcon"
+            :label="'Altura'"
+            :value="pokemonStore.pokemon.height"
+            :unit="'m'"
+          />
+          <PokemonData
+            :icon="CategoryIcon"
+            :label="'Categoría'"
+            :value="pokemonStore.pokemon.category.replace('Pokémon', '')"
+            :unit="''"
+          />
+          <PokemonData
+            :icon="HabilityIcon"
+            :label="'Habilidad'"
+            :value="pokemonStore.pokemon.abilities[0]"
+            :unit="''"
+          />
         </div>
         <div class="mt-4">
           <h3
@@ -131,8 +166,18 @@
               ></div>
             </div>
             <div class="flex justify-between items-center w-full">
-              <div class="flex justify-start items-center gap-2 text-[0.75rem] font-medium"><span><MaleIcon /></span><span> {{ pokemonStore.pokemon.gender.male }}%</span></div>
-              <div class="flex justify-start items-center gap-2 text-[0.75rem] font-medium"><span><FemaleIcon /></span><span>{{ pokemonStore.pokemon.gender.female }}%</span></div>
+              <div
+                class="flex justify-start items-center gap-2 text-[0.75rem] font-medium"
+              >
+                <span><MaleIcon /></span
+                ><span> {{ pokemonStore.pokemon.gender.male }}%</span>
+              </div>
+              <div
+                class="flex justify-start items-center gap-2 text-[0.75rem] font-medium"
+              >
+                <span><FemaleIcon /></span
+                ><span>{{ pokemonStore.pokemon.gender.female }}%</span>
+              </div>
             </div>
           </div>
 
@@ -145,11 +190,16 @@
           </h3>
 
           <ul class="flex gap-4 flex-wrap mt-2">
-            <li v-for="weakness in pokemonStore.pokemon.weaknesses" :key="weakness">
-              <PokemonTypeBadge 
-              :badgeStyle="pokemonTheme[weakness as keyof typeof pokemonTheme].badge"
-              :icon="pokemonTheme[weakness as keyof typeof pokemonTheme].icon"
-              :name="translatePokemonType(weakness)"
+            <li
+              v-for="weakness in pokemonStore.pokemon.weaknesses"
+              :key="weakness"
+            >
+              <PokemonTypeBadge
+                :badgeStyle="
+                  pokemonTheme[weakness as keyof typeof pokemonTheme].badge
+                "
+                :icon="pokemonTheme[weakness as keyof typeof pokemonTheme].icon"
+                :name="translatePokemonType(weakness)"
               />
             </li>
           </ul>
@@ -167,8 +217,10 @@ import { pokemonTheme } from "@/constants/pokemonTheme";
 import { translatePokemonType } from "@/constants/pokemonTypes";
 import PokemonData from "@/components/pokemons/PokemonData.vue";
 import PokemonTypeBadge from "@/components/pokemons/PokemonTypeBadge.vue";
-import Feedback from '@/components/common/Feedback.vue';
-import FavButton from '@/components/common/FavButton.vue';
+import Feedback from "@/components/common/Feedback.vue";
+import FavButton from "@/components/common/FavButton.vue";
+import IconButton from "@/components/common/IconButton.vue";
+import ShareIcon from "@/components/icons/ShareIcon.vue";
 import WeightIcon from "@/components/icons/WeightIcon.vue";
 import HeightIcon from "@/components/icons/HeightIcon.vue";
 import CategoryIcon from "@/components/icons/CategoryIcon.vue";
@@ -181,12 +233,9 @@ import FemaleIcon from "@/components/icons/FemaleIcon.vue";
 const route = useRoute();
 const pokemonStore = usePokemonStore();
 const loadedPokemonImage = ref(false);
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
-const router = useRouter()
-const goBack = () => {
-  router.back()
-}
+const router = useRouter();
 
 onMounted(async () => {
   loadedPokemonImage.value = false;
@@ -201,5 +250,39 @@ const primaryType = computed(() => {
 const theme = computed(() => {
   return pokemonTheme[primaryType.value as keyof typeof pokemonTheme];
 });
-</script>
 
+const capitalize = (pokemonInfo: string) =>
+  pokemonInfo.charAt(0).toUpperCase() + pokemonInfo.slice(1);
+
+const copyPokemonInfo = async () => {
+  if (!pokemonStore.pokemon) return;
+
+  const pokemon = pokemonStore.pokemon;
+
+  const pokemonInfo = [
+    `Nombre: ${capitalize(pokemon.name)}`,
+    `Tipos: ${pokemon.types
+      .map((type: string) => capitalize(translatePokemonType(type)))
+      .join(", ")}`,
+    `Peso: ${pokemon.weight} kg`,
+    `Altura: ${pokemon.height} m`,
+    `Categoría: ${capitalize(pokemon.category.replace("Pokémon", "").trim())}`,
+    `Habilidad: ${capitalize(pokemon.abilities[0])}`,
+    `Género masculino: ${
+      pokemon.gender ? `${pokemon.gender.male}%` : "Sin género"
+    }`,
+    `Género femenino: ${
+      pokemon.gender ? `${pokemon.gender.female}%` : "Sin género"
+    }`,
+    `Debilidades: ${pokemon.weaknesses
+      .map((type: string) => capitalize(translatePokemonType(type)))
+      .join(", ")}`,
+  ].join(", ");
+
+  try {
+    await navigator.clipboard.writeText(pokemonInfo);
+  } catch (error) {
+    console.error("No se pudo copiar al portapapeles", error);
+  }
+};
+</script>
