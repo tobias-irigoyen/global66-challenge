89c957d3f7bd2fde555e964be7faada964a660d0
PC <tobias.irigoyen@gmail.com>
2026-08-06 11:02:03 -0300

    (pokedemon card): adjust sizes

--- src/components/pokemons/PokemonCard.vue
+++ src/components/pokemons/PokemonCard.vue
@@ -8,7 +8,7 @@
     class="flex justify-between items-center w-full max-w-[328px]"
   >
   <article
-    class="flex justify-between items-center rounded-lg w-full"
+    class="flex justify-between items-center rounded-lg w-full cursor-pointer h-[102px]"
     :class="theme.card"
   >
     <div class="pl-4">
@@ -33,12 +33,12 @@
         </li>
       </ul>
     </div>
-    <div class="relative rounded-lg p-4 bg-no-repeat bg-center bg-[length:90%]" :class="theme.badge">
+    <div class="relative rounded-lg bg-no-repeat bg-center bg-[length:90%] h-[102px] w-[126px]" :class="theme.badge">
       <FavButton
         :pokemon="pokemon"
         class="absolute !right-[5px] top-[5px] z-20"
       />
-      <div class="relative z-10 w-[94px] h-[94px]">
+      <div class="relative z-10 w-[126px] h-[102px] flex items-center justify-center">
         <div
           v-if="!loadedPokemonImage"
           class="absolute inset-0 rounded-lg bg-gray-300 animate-pulse"
